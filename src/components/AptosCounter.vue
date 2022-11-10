a6f883e5e588927696f258481f14ceb174a942a5
Mohammed Najib Yahya <mohammed.njeeb@gmail.com>
2022-11-10 21:57:22 +0300

    connected frontend to wallet

--- src/components/AptosCounter.vue
+++ src/components/AptosCounter.vue
@@ -1,11 +1,48 @@
 <script setup lang="ts">
 import { AptosClient } from "aptos";
-const moveModule = import.meta.env.VITE_MOVE_MODULE_ADDRESS
+import { onBeforeMount, ref } from "vue";
+const moveModule = import.meta.env.VITE_MOVE_MODULE_ADDRESS;
 const client = new AptosClient("https://fullnode.devnet.aptoslabs.com/v1");
 
-console.log(await client.getAccountResources(moveModule));
+const count = ref(0);
+const walletAddress = ref("");
+
+const getCount = async () => {
+  const resources = await client.getAccountResources(walletAddress.value);
+  const resourceType = `${moveModule}::counter::CounterHolder`;
+  const resource = resources.find((e) => e.type === resourceType);
+  const count = (resource?.data as any).count;
+
+  return count;
+};
+
+const bump = async () => {
+  const transaction = {
+    type: "entry_function_payload",
+    function: `${moveModule}::counter::bump`,
+    arguments: [],
+    type_arguments: [],
+  };
+
+  const txHash = await (window as any).aptos.signAndSubmitTransaction(
+    transaction
+  );
+
+  console.log("txHash is: ", txHash);
+
+  count.value = await getCount();
+};
+
+onBeforeMount(async () => {
+  const wallet = await (window as any).aptos.connect();
+  walletAddress.value = wallet.address;
+  count.value = await getCount();
+});
 </script>
 
 <template>
-  <div>Some Text</div>
+  <div>
+    <p>Your count: {{ count }}</p>
+    <button @click="bump()">bump</button>
+  </div>
 </template>
